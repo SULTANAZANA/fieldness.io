b51fcc4fb81a99a8d77b99f84356f1a991091d8e
SULTANAZANA <36982021+SULTANAZANA@users.noreply.github.com>
2020-09-09 07:00:09 +0530

    Add files via upload

---
+++ hide.js
@@ -0,0 +1,74 @@
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
+function myFunction2() {
+    var x = document.getElementById("dapp");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function myFunction3() {
+    var x = document.getElementById("landscape");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction4() {
+    var x = document.getElementById("why");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction4i() {
+    var x = document.getElementById("whatis");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction5() {
+    var x = document.getElementById("onemin");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction6() {
+    var x = document.getElementById("morethan");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction7() {
+    var x = document.getElementById("where");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
